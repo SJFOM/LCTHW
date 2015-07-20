0b7897162e95f04458868eb030c18f084900c612
Sam-Mac <Sam@mac>
2015-07-20 14:21:18 +0200

    Updated stack.py

--- ex17/extra_credit/stack.py
+++ ex17/extra_credit/stack.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 class Stack:
-    def __init__(self):
+    def __init__(self, stack_size):
         self.nodes = []
 
     def push(self, value):
@@ -17,15 +17,15 @@ class Stack:
         return self.size() == 0
 
     def is_full(self):
-        return self.size() == 5
+        return self.size() == stack_size
 
     def size(self):
         return len(self.nodes)
 
+stack_size = 10
+stack = Stack(stack_size)
 
-stack = Stack()
-
-i=1
+i = 1
 while not stack.is_full():
     stack.push(i)
     i += 1
@@ -33,5 +33,4 @@ while not stack.is_full():
 while not stack.is_empty():
     print stack.top()
     stack._pop()
-
 
