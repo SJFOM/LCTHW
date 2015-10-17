2f7d10ba0b6df77aa6682c6db738203f3d003c28
sam-ubuntu <sam@ubuntu.desktop>
2015-10-17 19:12:51 +0100

    Function now takes both negative and positive numbers. Further optimization and testing required...

--- Testing/ARM/inc_number.c
+++ Testing/ARM/inc_number.c
@@ -4,45 +4,66 @@ int *inc_number(int number[]);
 
 int main(int argc, char *argv[])
 {
-	int number[4] = {1,2,3,4};
+	int number[4] = {-1,2,3,4};
 
-	//int value = number[0]*1000 + number[1]*100
-	//	+ number[2]*10 + number[3];
+	int value = number[0]*1000 + number[1]*100
+		+ number[2]*10 + number[3];
 
-	//printf("User input: %d\n", value);
+	printf("User input: %d\n", value);
 
-	int *back = inc_number(number);
+	int *output = inc_number(number);
 
-	printf("back: %d,%d,%d,%d\n", back[0], 
-		back[1], back[2], back[3]);
+	printf("output: %d,%d,%d,%d\n", output[0], 
+		output[1], output[2], output[3]);
 	return 0;
 }
 
 
 int *inc_number(int number[])
-{
-	register unsigned int flag = 0;
-	for(i=3; value > 0; i--) {
-		if(number[i] < 0){flag = 1;}
+{	
+	register unsigned int i = 0;
+	register unsigned int sign = 0;
+	int flag[4] = {1000,100,10,1};
+	
+	for(i = 0; i <= 3; i++) {
+		if(number[i] < 0){
+			flag[i] *= -1;
+			sign = 1;
+			break;
+		}
 	}
 
-	register unsigned int value = number[0]*1000 + number[1]*100
-		+ number[2]*10 + number[3] + 1;
+	// printf("flag = %d,%d,%d,%d\n", flag[0], flag[1],
+	// 		flag[2], flag[3]);
 
+
+	// Maybe try using the indexing of the previous for loop for the value and just += it as a sum?
 	//if(value > 9999) { value /= 10;}
 
-	//unsigned int output[] = {0,0,0,0};
+	register unsigned int value = number[0]*flag[0]
+	+ number[1]*flag[1]
+	+ number[2]*flag[2]
+	+ number[3]*flag[3];
 
-	register unsigned int i = 0;
+	if(sign == 0){
+		value++;
+	} else {
+		puts("Yep, I'm negative");
+		value--;
+		//value *= -1;
+		number[i] *= -1;
+	}
+
+	register unsigned int temp = i;
+
 	register unsigned int div = 10;
 
 	for(i=3; value > 0; i--) {
-		number[i] = value % div;
+		number[i] = value % div; 
 		value /= div;
 	}
 
-	//printf("output = %d%d%d%d\n", number[0], number[1],
-	//		number[2], number[3]);
-	
+	number[temp] *= -1;
+
 	return number;
 }
