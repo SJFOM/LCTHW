1323249c52baf3efa0cdb0d307b2a0f535911f78
Sam-Mac <Sam@mac>
2015-10-21 17:32:05 +0100

    Testing scripts done, just need to optimise the c code a little further...

--- Testing/ARM/inc_number.c
+++ Testing/ARM/inc_number.c
@@ -6,25 +6,15 @@ int *inc_number(int number[]);
 
 int main(int argc, char *argv[])
 {
-	// int number[4] = {argv[1][0] - '0',
-	// argv[2][1] - '0',
-	// argv[3][2] - '0',
-	// argv[4][3] - '0'};
-
-	// printf("input: %d,%d,%d,%d\n", number[0],
-	// 	number[1], number[2], number[3]);
-
 	int number[4] = {atoi(argv[1]),
 		atoi(argv[2]),
 		atoi(argv[3]),
 		atoi(argv[4])};
 
-
 		int *output = inc_number(number);
 
 		printf("%d%d%d%d\n", output[0], 
 			output[1], output[2], output[3]);
-
 
 		return 0;
 	}
@@ -53,7 +43,7 @@ int *inc_number(int number[])
 	+ number[2]*flag[2]
 	+ number[3]*flag[3];
 
-	printf("value %d\n", value);
+	// printf("value %d\n", value);
 
 	if(sign == 0){
 		// if(value == 9999){
@@ -70,25 +60,20 @@ int *inc_number(int number[])
 
 	//if(value == 0){number[3] = 1;}
 
-	printf("value %d\n", value);
+	// printf("value %d\n", value);
 
 	// value += (sign) ? -1 : 1;
 
 	//register unsigned int temp = i;
 
+	memset(number, 0, 4 * sizeof(number[0]));
+
 	register unsigned int div = 10;
 
 	for(i=3; (value > 0) && (i != -1); i--) {
-		printf("value %% div: %d\n", value % div);
-		printf("i: %d\n", i);
 		number[i] = value % div; 
 		value /= div;
 	}
-
-	memset(number, 0, 4 * sizeof(number[0]));
-
-	printf("number: %d,%d,%d,%d\n", number[0],
-		number[1], number[2], number[3]);
 
 	// i=3;
 	// while(value > 0 && i!=-1){
--- Testing/ARM/test_script.py
+++ Testing/ARM/test_script.py
@@ -1,60 +1,58 @@
 #!/usr/bin/env python
-
 import subprocess
+import sys
 
 # Set up the program variables
 PROGRAM = './inc_number'
 MIN_INPUT=-9999
-MAX_INPUT=9998
-div=10
+MAX_INPUT=9999
+indx = 0
+num = [0] * 4
 
-numA = [0] * 5
-numB = numA
+# Iterative Loop
 
-# Negative Loop
+for i in range(MIN_INPUT, MAX_INPUT+1):
 
-for i in range(7, 10):
+    j = i
+    sign = 0
 
-    for j in range(3,-1,-1):
-        numA[j] = abs(i-1) % div
-        numB[j] = abs(i) % div
-        i /= div
+    if i < 0:
+        sign = 1;
+        i = abs(i)
 
-    
-    print numA[3]
-    print numB[3]
+    temp = list(str(i))
 
-    # print [int(j) for j in str(i)]
-    
+    num = [0]*(4-len(temp))
+    num.extend(temp)
 
-    A = int(subprocess.Popen([PROGRAM, str(numA[0]), str(numA[1]),
-        str(numA[2]), str(numA[3])], stdout=subprocess.PIPE, 
+    num = map(int, num)
+
+    if sign == 1:
+        indx = next((i for i, x in enumerate(num) if x), -1)
+        num[indx] *= -1
+
+    num = map(str, num)
+
+    cmd = [PROGRAM, num[0], num[1], num[2], num[3]]
+
+    output = list(subprocess.Popen(cmd, stdout=subprocess.PIPE, 
         stderr=subprocess.PIPE).communicate()[0])
 
-    B = int(subprocess.Popen([PROGRAM, str(numB[0]), str(numB[1]),
-        str(numB[2]), str(numB[3])], stdout=subprocess.PIPE, 
-        stderr=subprocess.PIPE).communicate()[0])
+    if '-' in output: 
+        output.remove('-')
+        output = int(''.join(map(str,output)))*-1
+    else:
+        output = int(''.join(map(str,output)))
 
-    # print A
-    # print B
+    print "input: ", j
 
+    print "output: ", output, "\n"
 
-    if(B-A != 1):
-        print "ERROR: Integer difference is incorrect"
-        print B-A
-
-
-# print [str(j) for j in str(-100)]
-# print [int(char) for char in str(634)]
-
-# print list(str(-10))
-
-# numA *= 0
-
-# numA = list(str(-1))
-
-# print numA[0], numA[1], numA[2], numA[3], numA[4]
-
-
+    if output - j != 1:
+        print "*"*60
+        print "\nDifference = ", "(",output,") - (", j, ")",
+        ", should be 1."
+        sys.exit(""" Either integer difference is incorrect OR 
+            end of loop encountered.""")
 
 
